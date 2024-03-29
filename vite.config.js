e673df17d3d176d65c16cbd58be00e134c83344d
zy <1824762582@qq.com>
2024-03-29 22:28:41 +0800

    axios的导入及跨域问题的解决

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,18 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    proxy: {
+      //拦截所有包含/api的链接
+      '/api': {
+        //转换的目标链接将5173转为5017，由前端发送
+        target: 'http://localhost:5017',
+        //是否换源，true则转化
+        changeOrigin: true,
+        //使得转换后的链接中的/api变成空字符串
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })

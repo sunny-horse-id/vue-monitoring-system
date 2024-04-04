7fffef4edfe461ae9497534a772ab0dbbdd57b2b
zy <1824762582@qq.com>
2024-04-04 14:32:18 +0800

    分栏并添加表格的尝试

--- src/views/Home/components/HomeMain.vue
+++ src/views/Home/components/HomeMain.vue
@@ -2,10 +2,14 @@
 import * as echarts from 'echarts';
 
 setTimeout(() => {
+  init1();
   init2();
+  init3();
+  init4();
 }, 1);
-function init2() {
-  const chartDom = document.getElementById('main');
+// 左侧-折线图
+function init1() {
+  const chartDom = document.getElementById('init1');
   const myChart = echarts.init(chartDom);
   function randomData() {
     now = new Date(+now + oneDay);
@@ -86,11 +90,368 @@ function init2() {
 
   option && myChart.setOption(option);
 }
+
+// 左侧-柱状图
+function init2() {
+  const app = {};
+
+  const chartDom = document.getElementById('init2');
+  const myChart = echarts.init(chartDom);
+
+  const categories = (function () {
+    let now = new Date();
+    let res = [];
+    let len = 10;
+    while (len--) {
+      res.unshift(now.toLocaleTimeString().replace(/^\D*/, ''));
+      now = new Date(+now - 2000);
+    }
+    return res;
+  })();
+  const categories2 = (function () {
+    let res = [];
+    let len = 10;
+    while (len--) {
+      res.push(10 - len - 1);
+    }
+    return res;
+  })();
+  const data = (function () {
+    let res = [];
+    let len = 10;
+    while (len--) {
+      res.push(Math.round(Math.random() * 1000));
+    }
+    return res;
+  })();
+  const data2 = (function () {
+    let res = [];
+    let len = 0;
+    while (len < 10) {
+      res.push(+(Math.random() * 10 + 5).toFixed(1));
+      len++;
+    }
+    return res;
+  })();
+  const option = {
+    title: {
+      text: 'Dynamic Data'
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'cross',
+        label: {
+          backgroundColor: '#283b56'
+        }
+      }
+    },
+    legend: {},
+    toolbox: {
+      show: true,
+      feature: {
+        dataView: { readOnly: false },
+        restore: {},
+        saveAsImage: {}
+      }
+    },
+    dataZoom: {
+      show: false,
+      start: 0,
+      end: 100
+    },
+    xAxis: [
+      {
+        type: 'category',
+        boundaryGap: true,
+        data: categories
+      },
+      {
+        type: 'category',
+        boundaryGap: true,
+        data: categories2
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value',
+        scale: true,
+        name: 'Price',
+        max: 30,
+        min: 0,
+        boundaryGap: [0.2, 0.2]
+      },
+      {
+        type: 'value',
+        scale: true,
+        name: 'Order',
+        max: 1200,
+        min: 0,
+        boundaryGap: [0.2, 0.2]
+      }
+    ],
+    series: [
+      {
+        name: 'Dynamic Bar',
+        type: 'bar',
+        xAxisIndex: 1,
+        yAxisIndex: 1,
+        data: data
+      },
+      {
+        name: 'Dynamic Line',
+        type: 'line',
+        data: data2
+      }
+    ]
+  };
+  app.count = 11;
+  setInterval(function () {
+    let axisData = new Date().toLocaleTimeString().replace(/^\D*/, '');
+    data.shift();
+    data.push(Math.round(Math.random() * 1000));
+    data2.shift();
+    data2.push(+(Math.random() * 10 + 5).toFixed(1));
+    categories.shift();
+    categories.push(axisData);
+    categories2.shift();
+    categories2.push(app.count++);
+    myChart.setOption({
+      xAxis: [
+        {
+          data: categories
+        },
+        {
+          data: categories2
+        }
+      ],
+      series: [
+        {
+          data: data
+        },
+        {
+          data: data2
+        }
+      ]
+    });
+  }, 2100);
+
+  option && myChart.setOption(option);
+}
+
+// 中间-饼图
+function init3() {
+  const chartDom = document.getElementById('init3');
+  const myChart = echarts.init(chartDom);
+
+  const option = {
+    title: {
+      text: 'Nightingale Chart',
+      subtext: 'Fake Data',
+      left: 'center'
+    },
+    tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b} : {c} ({d}%)'
+    },
+    legend: {
+      left: 'center',
+      top: 'bottom',
+      data: [
+        'rose1',
+        'rose2',
+        'rose3',
+        'rose4',
+        'rose5',
+        'rose6',
+        'rose7',
+        'rose8'
+      ]
+    },
+    toolbox: {
+      show: true,
+      feature: {
+        mark: { show: true },
+        dataView: { show: true, readOnly: false },
+        restore: { show: true },
+        saveAsImage: { show: true }
+      }
+    },
+    series: [
+      {
+        name: 'Radius Mode',
+        type: 'pie',
+        radius: [20, 140],
+        center: ['25%', '50%'],
+        roseType: 'radius',
+        itemStyle: {
+          borderRadius: 5
+        },
+        label: {
+          show: false
+        },
+        emphasis: {
+          label: {
+            show: true
+          }
+        },
+        data: [
+          { value: 40, name: 'rose 1' },
+          { value: 33, name: 'rose 2' },
+          { value: 28, name: 'rose 3' },
+          { value: 22, name: 'rose 4' },
+          { value: 20, name: 'rose 5' },
+          { value: 15, name: 'rose 6' },
+          { value: 12, name: 'rose 7' },
+          { value: 10, name: 'rose 8' }
+        ]
+      },
+      {
+        name: 'Area Mode',
+        type: 'pie',
+        radius: [20, 140],
+        center: ['75%', '50%'],
+        roseType: 'area',
+        itemStyle: {
+          borderRadius: 5
+        },
+        data: [
+          { value: 30, name: 'rose 1' },
+          { value: 28, name: 'rose 2' },
+          { value: 26, name: 'rose 3' },
+          { value: 24, name: 'rose 4' },
+          { value: 22, name: 'rose 5' },
+          { value: 20, name: 'rose 6' },
+          { value: 18, name: 'rose 7' },
+          { value: 16, name: 'rose 8' }
+        ]
+      }
+    ]
+  };
+  option && myChart.setOption(option);
+}
+
+// 中间-折线图
+function init4() {
+  const chartDom = document.getElementById('init4');
+  const myChart = echarts.init(chartDom);
+
+  const option = {
+    title: {
+      text: 'Stacked Area Chart'
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'cross',
+        label: {
+          backgroundColor: '#6a7985'
+        }
+      }
+    },
+    legend: {
+      data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}
+      }
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: 'category',
+        boundaryGap: false,
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value'
+      }
+    ],
+    series: [
+      {
+        name: 'Email',
+        type: 'line',
+        stack: 'Total',
+        areaStyle: {},
+        emphasis: {
+          focus: 'series'
+        },
+        data: [120, 132, 101, 134, 90, 230, 210]
+      },
+      {
+        name: 'Union Ads',
+        type: 'line',
+        stack: 'Total',
+        areaStyle: {},
+        emphasis: {
+          focus: 'series'
+        },
+        data: [220, 182, 191, 234, 290, 330, 310]
+      },
+      {
+        name: 'Video Ads',
+        type: 'line',
+        stack: 'Total',
+        areaStyle: {},
+        emphasis: {
+          focus: 'series'
+        },
+        data: [150, 232, 201, 154, 190, 330, 410]
+      },
+      {
+        name: 'Direct',
+        type: 'line',
+        stack: 'Total',
+        areaStyle: {},
+        emphasis: {
+          focus: 'series'
+        },
+        data: [320, 332, 301, 334, 390, 330, 320]
+      },
+      {
+        name: 'Search Engine',
+        type: 'line',
+        stack: 'Total',
+        label: {
+          show: true,
+          position: 'top'
+        },
+        areaStyle: {},
+        emphasis: {
+          focus: 'series'
+        },
+        data: [820, 932, 901, 934, 1290, 1330, 1320]
+      }
+    ]
+  };
+
+  option && myChart.setOption(option);
+}
 </script>
 
 <template>
   <div class="home-main">
-    <div id="main" style="height: 500px; width: 500px"></div>
+    <el-row>
+      <el-col :span="8">
+        <div id="init1" style="height: 350px; width: 550px"></div>
+        <div id="init2" style="height: 350px; width: 500px"></div>
+      </el-col>
+      <el-col :span="10">
+        <div id="init3" style="height: 300px; width: 600px"></div>
+        <div id="init4" style="height: 400px; width: 600px"></div>
+      </el-col>
+      <el-col :span="6">
+        999
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -99,5 +460,6 @@ function init2() {
   position: fixed;
   top: 48px;
   left: 0;
+  width: 100%;
 }
 </style>

952d2b2dbaaaed891a097150aefb9a440551688a
zy <1824762582@qq.com>
2024-04-05 15:05:23 +0800

    调整标题字体大小

--- src/views/Deploy.vue
+++ src/views/Deploy.vue
@@ -3,14 +3,46 @@
 </script>
 
 <template>
-  <el-card class="page-container">
-    66666666666666666666666666666666
-  </el-card>
+  <div class="page-container">
+    <el-row :gutter="8">
+      <el-col :span="5">
+        <el-card>
+          666
+        </el-card>
+      </el-col>
+      <el-col :span="14">
+        <el-card>
+          777
+        </el-card>
+      </el-col>
+      <el-col :span="5">
+        <el-card>
+          888
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-row :gutter="8" class="el-row-footer">
+      <el-col :span="5">
+        <el-card>
+          666
+        </el-card>
+      </el-col>
+      <el-col :span="19">
+        <el-card>
+          777
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <style scoped>
 .page-container {
   min-height: 100%;
   box-sizing: border-box;
+
+  .el-row-footer {
+    margin-top: 8px;
+  }
 }
 </style>

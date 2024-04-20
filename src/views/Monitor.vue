14537c64a8c73af03e5e010c0dee3746c9b072d7
zy <1824762582@qq.com>
2024-04-20 09:35:05 +0800

    柱状图修改

--- src/views/Monitor.vue
+++ src/views/Monitor.vue
@@ -2,6 +2,24 @@
 import {ref} from "vue";
 import * as echarts from 'echarts';
 
+// 使用按钮控制柱状图
+const isShow = ref(true)
+const select = (index) => {
+  isShow.value = index
+  switch (index) {
+    case true:
+      setTimeout(() => {
+        getErrChart();
+      }, 1);
+      break
+    case false:
+      setTimeout(() => {
+        getErrTotalChart();
+      }, 1);
+      break
+  }
+}
+
 // 日期常量
 const times = ref(['00:00', '00:10', '00:20', '00:30', '00:40', '00:50', '01:00', '01:10', '01:20', '01:30', '01:40', '01:50', '02:00',
   '02:10', '02:20', '02:30', '02:40', '02:50', '03:00'])
@@ -370,17 +388,14 @@ function getErrChart() {
   const chartDom = document.getElementById('Err');
   const myChart = echarts.init(chartDom);
   const option = {
-    title: {
-      text: '季度故障分布',
-      left: '20px'
-    },
     legend: {
-      right: '55px'
+      top: '5px',
+      right: '45px'
     },
     tooltip: {},
     dataset: {
       source: [
-        ['季度', '故障1', '故障2', '故障3', '故障4', '故障5'],
+        ['季度', '发电侧电压', '发电测电流', '氢气罐压力', '储能侧健康度', '流体浓度梯度'],
         ['1', 73, 55, 66, 32, 44],
         ['2', 82, 65, 61, 26, 47],
         ['3', 53, 39, 55, 38, 41],
@@ -426,6 +441,33 @@ function getErrChart() {
   option && myChart.setOption(option);
 }
 
+function getErrTotalChart() {
+  const chartDom = document.getElementById('ErrTotal');
+  const myChart = echarts.init(chartDom);
+  const option = {
+    tooltip: {},
+    dataset: {
+      source: [
+        ['季度', '总数'],
+        ['1', 273],
+        ['2', 265],
+        ['3', 210],
+        ['4', 150],
+      ]
+    },
+    xAxis: {type: 'category'},
+    yAxis: {},
+    series: [
+      {
+        type: 'bar',
+        itemStyle: {
+          color: '#62D4CA' // 修改第一个柱子的颜色
+        }
+      },
+    ]
+  }
+  option && myChart.setOption(option);
+}
 // 初始化加载绘制的图表
 setTimeout(() => {
   getErrChart();
@@ -439,7 +481,8 @@ setTimeout(() => {
         placeholder="2023-04-21"
         style="width: 115px;position: absolute;z-index: 999; top:85px"
     />
-    <el-table :data="totalData" style="width: 100%; position: relative; top: -5px" height="313px" :row-style="{height:'55px'}">
+    <el-table :data="totalData" style="width: 100%; position: relative; top: -5px" height="313px"
+              :row-style="{height:'55px'}">
       <el-table-column fixed prop="name" width="115"/>
       <el-table-column v-for="(item, index) in times" :key="index" :prop="'date_' + index" :label="item" width="100"/>
     </el-table>
@@ -452,7 +495,8 @@ setTimeout(() => {
     />
     <el-col :span="15">
       <el-card style="height: 298px">
-        <el-table :data="errData" style="width: 100%; position: relative; top: -10px" height="290px" :row-style="{height:'50px'}">
+        <el-table :data="errData" style="width: 100%; position: relative; top: -10px" height="290px"
+                  :row-style="{height:'50px'}">
           <el-table-column fixed prop="name" width="115"/>
           <el-table-column v-for="(item, index) in times" :key="index" :prop="'date_' + index" :label="item"
                            width="100"/>
@@ -461,7 +505,14 @@ setTimeout(() => {
     </el-col>
     <el-col :span="9">
       <el-card style="height: 298px">
-        <div id="Err" style="width: 115%; height: 320px; position: relative; left: -20px;"></div>
+        <el-container style="position: relative; top: -5px">
+          <el-button :type="isShow ? 'success' : 'primary'" :plain="!isShow" @click="select(true)" style="z-index: 999">季度故障详细分布</el-button>
+          <el-button :type="!isShow ? 'success' : 'primary'" :plain="isShow" @click="select(false)" style="z-index: 999">季度故障总计</el-button>
+        </el-container>
+        <div v-if="isShow===true" id="Err"
+             style="width: 115%; height: 285px; position: relative; left: -20px; top:10px"></div>
+        <div v-if="isShow===false" id="ErrTotal"
+             style="width: 115%; height: 310px; position: relative; left: -20px; top:-30px"></div>
       </el-card>
     </el-col>
   </el-row>
